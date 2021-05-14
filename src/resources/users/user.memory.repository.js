baf1d12ce6b3c8481f5481d626167bb64d1ef910
MorzhaloPavel <morzhalo95@mail.ru>
2021-05-14 18:03:01 +0300

    feat: add getall get post delete

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,30 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
-};
+// const User = require('./user.model')
 
-module.exports = { getAll };
+let DB = [
+  {id: 1, name: 'Test', login: 'test', password: 'test'},
+  {id: 2, name: 'Admin', login: 'admin', password: 'admin'},
+  {id: 3, name: 'Test12', login: 'test12', password: 'test12'},
+]
+
+const getAll = () => DB;
+
+const get = async id => {
+  const user = await DB.filter(us => us.id === id)
+  if (!user) {
+    console.log('Error');
+  }
+  return user[0]
+}
+
+const save = async user => {
+  DB = [...DB, user]
+  return user
+}
+
+const remove = async id => {
+  DB = await DB.filter(us => us.id !== id)
+
+  return 'Seccess'
+}
+
+module.exports = { getAll, get, save, remove };

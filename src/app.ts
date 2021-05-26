4abe8f5084cfb5980fe92d64b3c8c28d4260905f
MorzhaloPavel <morzhalo95@mail.ru>
2021-05-26 11:49:11 +0300

    feat: change all file src

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+export {}
 const express = require('express');
 const swaggerUI = require('swagger-ui-express');
 const path = require('path');
@@ -6,6 +7,10 @@ const userRouter = require('./resources/users/user.router.ts');
 const boardsRouter = require('./resources/boards/boards.router.ts');
 const tasksRouter = require('./resources/tasks/tasks.router.ts');
 
+const request = express.Request()
+const response = express.Response()
+const nextFunction = express.NextFunction()
+
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
@@ -13,7 +18,7 @@ app.use(express.json());
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-app.use('/', (req, res, next) => {
+app.use('/', (req: typeof request, res: typeof response, next: typeof nextFunction) => {
   if (req.originalUrl === '/') {
     res.send('Service is running!');
     return;
